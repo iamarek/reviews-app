08179a71015b343854b96a526ff1a3cbb56b8007
Arek Chatys <arek.chatys@farewill.com>
2023-07-14 20:33:21 +0100

    Move BASE_URL to env variable

--- src/constants.ts
+++ src/constants.ts
@@ -1,4 +1,4 @@
-export const BASE_URL = "http://localhost:3000"
+export const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL
 
 export const PATHS = {
   PAGES: {
@@ -6,6 +6,6 @@ export const PATHS = {
     RESULTS: "/results",
   },
   API: {
-    REVIEWS: "/api/reviews1",
+    REVIEWS: "/api/reviews",
   },
 }
